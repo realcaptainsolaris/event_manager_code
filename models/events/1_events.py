c69295812d767631cb731bbac2aaf14454b419c3
bf solaris <hello@spielprinzip.com>
2022-10-29 01:59:45 +0200

    update event model

--- models/events/1_events.py
+++ models/events/1_events.py
@@ -1,18 +1,20 @@
-    from django.db import models
+from django.db import models
 
-    class DateMixin(models.Model):
-        """eine abstrakte Klasse, die selbst keine DB-Tabelle erstellt"""
-        created_at = models.DateTimeField(auto_now_add=True)
-        updated_at = models.DateTimeField(auto_now=True)
 
-        class Meta:
-            abstract = True
+class DateMixin(models.Model):
+    """eine abstrakte Klasse, die selbst keine DB-Tabelle erstellt"""
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    class Category(DateMixin):
-        """Eine Kategorie für einen Event."""
-        name = models.CharField(max_length=100)
-        sub_title = models.CharField(max_length=200, null=True, blank=True)
-        description = models.TextField(null=True, blank=True)
+    class Meta:
+        abstract = True
 
-        def __str__(self):
-            return self.name
+
+class Category(DateMixin):
+    """Eine Kategorie für einen Event."""
+    name = models.CharField(max_length=100)
+    sub_title = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
