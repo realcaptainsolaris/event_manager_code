a029eca490ea770cfa133f2a2f52c0564aae71cd
Captain Solaris <hello@spielprinzip.com>
2023-03-06 21:12:15 +0100

    category update

---
+++ views/category_add_1.py
@@ -0,0 +1,18 @@
+from django.shortcuts import get_object_or_404, render, reverse, redirect
+from .forms import CategoryForm
+
+
+def category_create(request):
+    """Eine View zum Hinzufügen einer Kategorie."""
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            category = form.save()
+            return redirect(category)
+    else:
+        form = CategoryForm()
+    return render(
+        request,
+        "events/category_add.html",
+        {"form": form},
+    )
---
+++ views/category_update_1.py
@@ -0,0 +1,14 @@
+def category_update(request, pk):
+    """View zum Ändern einer Kategorie."""
+    instance = get_object_or_404(Category, pk=pk)
+    form = CategoryForm(request.POST or None, instance=instance)
+
+    if form.is_valid():
+        category = form.save()
+        return redirect(category)
+
+    return render(
+        request,
+        "events/category_add.html",
+        {"form": form},
+    )
